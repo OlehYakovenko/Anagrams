d1ecffaaf94b91c7b1aa01d4d5eae1938762e71b
Oleg Iakovenko <iakovenkoua@gmail.com>
2019-03-12 16:19:12 +0200

    temp

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        //TODO enter phrases for todo anagram
+        // gitignore
+        // push from console
+        // rename
 
         String temp = Anagrams.doRev("asdfg fasd");
 
